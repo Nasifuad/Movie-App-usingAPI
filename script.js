ab51829ee3f74af63c7448df431412aa802acdbc
Nasif Fuad <nasifuad007@gmail.com>
2024-09-09 06:20:00 +0600

    Comment added

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Adding comments for the project
+
+//The api key = db865b4d295f7629dedf31bf09848745. from themoviebd.org. below the three url are for showing movies and image.
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=db865b4d295f7629dedf31bf09848745&page=1";
-
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=04c35731a5ee918f014970082a0088b1&query="';
 document.addEventListener("DOMContentLoaded", () => {
